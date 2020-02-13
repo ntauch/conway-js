f58ea28a7ac847a3fe47904dfc4151cc6fb9a29b
Nikolai Tauch <n.tauch@bauer-kirch.de>
2020-02-13 10:17:23 +0100

    Initial commit

---
+++ conway.js
@@ -0,0 +1,7 @@
+function init() {
+    let canvas = document.getElementById('canvasBoard');
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#6eff08';
+    ctx.fillRect(0,0,150,75);
+}
+
